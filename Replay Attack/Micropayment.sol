d07514290083066d69ae6bcbbaa53d339bcfa7bf
0xAlexF <102248301+0xAlexF@users.noreply.github.com>
2022-11-17 12:59:21 -0500

    Update Micropayment.sol

--- Replay Attack/Micropayment.sol
+++ Replay Attack/Micropayment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract ReceiverPays {
+contract Micropayment {
     address owner = msg.sender;
     //mapping(uint256 => bool) usedNonces;                                                         
 
